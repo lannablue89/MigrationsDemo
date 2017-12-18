4d6bcd3dcd1e0bbae026e54a13aa6c8b7216e424
lanna <lanna.blue89@gmail.com>
2017-12-18 08:46:33 +0700

    [ADD] add unit test project

---
+++ MediacorpSpellingGame-Server/UnitTestProject/DbInitUT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MediacorpSpellingGame_Server.Models;
+using System.Linq;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class DbInitUT
+    {
+        [TestMethod]
+        public void CanInitializeDatabase()
+        {
+            using (var context = new GameContext())
+            {
+                var rounds = context.Rounds.ToList();
+                foreach (var round in rounds)
+                {
+                    Console.WriteLine(round.ToString());
+                }
+            }
+            Assert.Inconclusive("If we're here, it did not crash");
+        }
+    }
+}
